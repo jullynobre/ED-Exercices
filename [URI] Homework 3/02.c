39b005f7e6e2c4809a09cc7747cbffd6bbd4d5df
jullynobre <jullynobre1@gmail.com>
2018-02-28 21:42:02 -0300

    Add solution to problem 1548

--- [URI] Homework 3/02.c
+++ [URI] Homework 3/02.c
@@ -1,37 +1,45 @@
+//https://www.urionlinejudge.com.br/judge/pt/problems/view/1548
+
 #include <stdio.h>
 
-int correctPositions(int *p, int m){
-	int *o, i, j, qtt = 0, bigger = 0;
-	o = malloc(m * sizeof(int));
-	for(i = 0; i < m; i++)
+int correctPositions(int *p, int *o, int m)
+{
+	int i, j, qtt = 0, aux = 0;
+
+	for(i = 0; i < m - 1; i++)
 	{
-		bigger = *(p + i);
-		for(j = i + i; j < m; j++)
+		for(j = i + 1; j < m; j++)
 		{
-			if(*(p + j) > *(p + i)) bigger = *(p + j);
+			if(*(o + i) < *(o + j)){
+				aux = *(o + j);
+				*(o + j) = *(o + i);
+				*(o + i) = aux;
+			}
 		}
-		*(o + i) = bigger;
 		if(*(o + i) == *(p + i)) qtt++;
 	}
+	if(*(o + m - 1) == *(p + m - 1)) qtt++;
 
 	return qtt;
 }
 
 int main()
 {
-	int n = 0, i, j, m = 0, *p;
+	int n = 0, i, j, m = 0, *p, *o;
 	scanf("%d", &n);
 
 	for(i = 0; i < n; i++)
 	{
 		scanf("%d", &m);
 		p = malloc(m*sizeof(int));
-		for(j = 0; j < n; j++)
+		o = malloc(m*sizeof(int));
+		for(j = 0; j < m; j++)
 		{
 			scanf("%d", (p + j));
+			*(o + j) = *(p + j);
 		}
 
-		printf("%d\n", correctPositions(p, m));
+		printf("%d\n", correctPositions(p, o, m));
 	}
 
 	system("pause");
