f67ff9cfe0e3d976e2d3fac2eb577b659c30fbc8
jullynobre <jullynobre1@gmail.com>
2018-05-19 17:04:57 -0300

    Add method insert to BinaryTree

--- BasicDataStructuresImplementation/BinaryTree.c
+++ BasicDataStructuresImplementation/BinaryTree.c
@@ -13,3 +13,27 @@ typedef struct aux {
 
     struct aux *left, *right;
 } NODE;
+
+typedef NODE* POINTER;
+
+//Function that initialize a tree; Outher functionalities can be added to this function in future
+POINTER init(){
+	return(NULL);
+}
+
+int main(){
+	POINTER r = init();
+	return(0);
+}
+
+POINTER insert(POINTER root, POINTER node){
+	if(root == NULL){
+		return(node);
+	}
+	if(node->key < root->key){
+		root->left = insert(root->left, node);
+	}else{
+		root->right = insert(root->right, node);
+	}
+	return(root);
+}
